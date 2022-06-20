7e8c53b3ff71c6fcd7c6c7628816de81a29b93e0
Bryan <bryanagan25@gmail.com>
2022-06-20 20:17:12 +0800

    Added decision output for the intro buttons

--- GWA Application/GWA Main.py
+++ GWA Application/GWA Main.py
@@ -2,8 +2,15 @@ import tkinter as tk
 
 
 def intro_buttons(window):
-    update_button = tk.Button(window, text="Update", font=("Helvetica", 25)).grid(row=0, padx=(90, 100), pady=120)
-    calculate_button = tk.Button(window, text="Calculate", font=("Helvetica", 25)).grid(row=0, padx=(30, 100), column=1)
+    update_button = tk.Button(window, text="Update", font=("Helvetica", 25), command=lambda: intro_decision('update')).grid(row=0, padx=(90, 100), pady=120)
+    calculate_button = tk.Button(window, text="Calculate", font=("Helvetica", 25), command=lambda: intro_decision('calculate')).grid(row=0, padx=(30, 100), column=1)
+
+
+def intro_decision(decision):
+    if decision == 'update':
+        print('update')
+    elif decision == 'calculate':
+        print('calculate')
 
 
 def main():
