3b3b8319846806997ff6f564859d7f8e2ed3e60f
Bryan <bryanagan25@gmail.com>
2022-06-20 21:41:58 +0800

    Added grade level decisions on the update portion

--- GWA Application/GWA Main.py
+++ GWA Application/GWA Main.py
@@ -14,14 +14,43 @@ class IntroButtons:
         if decision == 'update':
             print('update')
             self.window.config(bg='white')
-            self.update_button.config(state='disabled')
-            self.calculate_button.config(state='active')
+            self.update_button.grid_remove()
+            self.calculate_button.grid_remove()
+            update_buttons = UpdateButtons(self.window)
 
         elif decision == 'calculate':
             print('calculate')
             self.window.config(bg='black')
-            self.update_button.config(state='active')
-            self.calculate_button.config(state='disabled')
+            self.update_button.grid_remove()
+            self.calculate_button.grid_remove()
+
+
+class UpdateButtons:
+    def __init__(self, window):
+        # Grade levels
+        self.window = window
+        self.g7_button = tk.Button(window, text="Grade 7 ", font=("Helvetica", 20), command=lambda: self.grade_decision('7'))
+        self.g7_button.grid(row=0, padx=(90, 100), pady=20)
+        self.g8_button = tk.Button(window, text="Grade 8 ", font=("Helvetica", 20), command=lambda: self.grade_decision('8'))
+        self.g8_button.grid(row=0, padx=(30, 100), column=1)
+        self.g9_button = tk.Button(window, text="Grade 9 ", font=("Helvetica", 20), command=lambda: self.grade_decision('9'))
+        self.g9_button.grid(row=1, padx=(90, 100), pady=20)
+        self.g10_button = tk.Button(window, text="Grade 10", font=("Helvetica", 20), command=lambda: self.grade_decision('10'))
+        self.g10_button.grid(row=1, padx=(35, 100), column=1)
+        self.g11_button = tk.Button(window, text="Grade 11", font=("Helvetica", 20), command=lambda: self.grade_decision('11'))
+        self.g11_button.grid(row=2, padx=(90, 100), pady=20)
+        self.g12_button = tk.Button(window, text="Grade 12", font=("Helvetica", 20), command=lambda: self.grade_decision('12'))
+        self.g12_button.grid(row=2, padx=(35, 100), column=1)
+
+    def grade_decision(self, decision):
+        if decision == '7':
+            self.g7_button.grid_remove()
+            self.g8_button.grid_remove()
+            self.g9_button.grid_remove()
+            self.g10_button.grid_remove()
+            self.g11_button.grid_remove()
+            self.g12_button.grid_remove()
+
 
 
 def main():
