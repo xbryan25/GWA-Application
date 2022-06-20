d58053609ff8d131b5edb8d0a9158b334cf1fa3b
Bryan <bryanagan25@gmail.com>
2022-06-20 20:50:03 +0800

    Turned intro decision function into a method of the intro buttons class, also improved intro decision function

--- GWA Application/GWA Main.py
+++ GWA Application/GWA Main.py
@@ -1,18 +1,27 @@
 import tkinter as tk
 
 
-class Buttons:
+class IntroButtons:
     def __init__(self, window):
         # Intro buttons
-        self.update_button = tk.Button(window, text="Update", font=("Helvetica", 25), command=lambda: intro_decision('update')).grid(row=0, padx=(90, 100), pady=120)
-        self.calculate_button = tk.Button(window, text="Calculate", font=("Helvetica", 25), command=lambda: intro_decision('calculate')).grid(row=0, padx=(30, 100), column=1)
+        self.window = window
+        self.update_button = tk.Button(window, text="Update", font=("Helvetica", 25), command=lambda: self.intro_decision('update'))
+        self.update_button.grid(row=0, padx=(90, 100), pady=120)
+        self.calculate_button = tk.Button(window, text="Calculate", font=("Helvetica", 25), command=lambda: self.intro_decision('calculate'))
+        self.calculate_button.grid(row=0, padx=(30, 100), column=1)
 
+    def intro_decision(self, decision):
+        if decision == 'update':
+            print('update')
+            self.window.config(bg='white')
+            self.update_button.config(state='disabled')
+            self.calculate_button.config(state='active')
 
-def intro_decision(decision):
-    if decision == 'update':
-        print('update')
-    elif decision == 'calculate':
-        print('calculate')
+        elif decision == 'calculate':
+            print('calculate')
+            self.window.config(bg='black')
+            self.update_button.config(state='active')
+            self.calculate_button.config(state='disabled')
 
 
 def main():
@@ -24,7 +33,7 @@ def main():
     window.configure(background="black")
     window.geometry('600x300')
 
-    buttons = Buttons(window)
+    buttons = IntroButtons(window)
 
     window.mainloop()
 
