bd32c17050f18e1d601027dcd367a4eb05ca9304
Bryan <bryanagan25@gmail.com>
2022-06-20 20:13:05 +0800

    Added intro buttons

--- GWA Application/GWA Main.py
+++ GWA Application/GWA Main.py
@@ -1,11 +1,21 @@
 import tkinter as tk
 
 
+def intro_buttons(window):
+    update_button = tk.Button(window, text="Update", font=("Helvetica", 25)).grid(row=0, padx=(90, 100), pady=120)
+    calculate_button = tk.Button(window, text="Calculate", font=("Helvetica", 25)).grid(row=0, padx=(30, 100), column=1)
+
+
 def main():
     window = tk.Tk()
+
+    # Window configurations
     window.title("GWA Application by Bryan")
     window.resizable(width='FALSE', height='FALSE')
+    window.configure(background="black")
     window.geometry('600x300')
+
+    intro_buttons(window)
 
     window.mainloop()
 
