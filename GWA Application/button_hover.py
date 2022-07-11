049e1e0efe50fc3f9458e257fcc505348bffd2cf
Bryan <bryanagan25@gmail.com>
2022-07-11 21:50:11 +0800

    Update ButtonHover class

--- GWA Application/button_hover.py
+++ GWA Application/button_hover.py
@@ -1,12 +1,23 @@
 
 class ButtonHover:
-    def __init__(self, button):
+    def __init__(self, button, button_type):
         self.button = button
+        self.button_type = button_type
 
     def on_enter(self, entry):
-        self.button['background'] = '#CFCFCF'
-        self.button['foreground'] = 'black'
+        if self.button_type == 'calculate' or self.button_type == 'credits':
+            self.button['background'] = '#CFCFCF'
+            self.button['foreground'] = 'black'
+
+        elif self.button_type == 'back_button':
+            self.button['background'] = '#DB5F44'
+            self.button['foreground'] = 'black'
 
     def on_leave(self, entry):
-        self.button['background'] = 'white'
-        self.button['foreground'] = 'black'
+        if self.button_type == 'calculate' or 'credits':
+            self.button['background'] = 'white'
+            self.button['foreground'] = 'black'
+
+        elif self.button_type == 'back_button':
+            self.button['background'] = 'white'
+            self.button['foreground'] = 'black'
