57333cbf72d4c9873f5e118b377319de5f253d2b
Bryan <bryanagan25@gmail.com>
2022-07-16 21:09:24 +0800

    Update ButtonHover class

--- GWA Application/button_hover.py
+++ GWA Application/button_hover.py
@@ -28,39 +28,39 @@ class ButtonHover:
             self.button['background'] = '#9F9E9D'
             self.button['foreground'] = 'black'
 
-        elif self.button_type == 'g7_button':
+        elif self.button_type == 'subject_button':
             if self.sign == 'minus':
-                self.button['background'] = '#DB5F44'
+                self.button['background'] = '#FF5649'
                 self.button['foreground'] = 'black'
-            if self.sign == 'add':
-                self.button['background'] = '#DB5F44'
+            elif self.sign == 'add':
+                self.button['background'] = '#78E93D'
                 self.button['foreground'] = 'black'
 
     def on_leave(self, entry):
-        if self.button_type == 'calculate' or 'credits':
-            self.button['background'] = 'white'
+        if self.button_type == 'calculate' or self.button_type == 'credits':
+            self.button['background'] = '#E2E2E2'
             self.button['foreground'] = 'black'
 
         elif self.button_type == 'back_button':
-            self.button['background'] = 'white'
+            self.button['background'] = '#E2E2E2'
             self.button['foreground'] = 'black'
 
         elif self.button_type == 'grade_level_button':
-            self.button['background'] = 'white'
+            self.button['background'] = '#E2E2E2'
             self.button['foreground'] = 'black'
 
         elif self.button_type == 'save_button':
-            self.button['background'] = 'white'
+            self.button['background'] = '#E2E2E2'
             self.button['foreground'] = 'black'
 
         elif self.button_type == 'import_button':
-            self.button['background'] = 'white'
+            self.button['background'] = '#E2E2E2'
             self.button['foreground'] = 'black'
 
-        elif self.button_type == 'g7_button':
+        elif self.button_type == 'subject_button':
             if self.sign == 'minus':
-                self.button['background'] = 'white'
+                self.button['background'] = '#E2E2E2'
                 self.button['foreground'] = 'black'
-            if self.sign == 'add':
-                self.button['background'] = 'white'
+            elif self.sign == 'add':
+                self.button['background'] = '#E2E2E2'
                 self.button['foreground'] = 'black'
